b3ae782499573fe6c9900326319d7a3c1fe1c7d5
MellerinCharly <mellerincharly.pro@gmail.com>
2024-10-29 11:23:51 +0100

    Creation of the cards of horeur films with the synopsis button that shows in fade a lorem

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Card from "../src/components/Card";
+import Header from "../src/components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const cardsData = [
+		{
+			firstname: "Vendredi 13",
+			imgSrc: "https://m.media-amazon.com/images/I/710oUcADbhL.jpg"
+		},
+		{
+			firstname: "Les griffes de la Nuit",
+			imgSrc: "https://www.ecranlarge.com/content/uploads/2020/03/les-griffes-de-la-nuit-affiche-francaise-1171041.png"
+		},
+		{
+			firstname: "La Mouche",
+			imgSrc: "https://fr.web.img6.acsta.net/medias/nmedia/18/65/16/97/18952914.jpg"
+		},
+		{
+			firstname: "Silent Hill",
+			imgSrc: "https://m.media-amazon.com/images/I/411jTQd6O0L._AC_UF1000,1000_QL80_.jpg"
+		},
+		{
+			firstname: "Dead Silence",
+			imgSrc: "https://musicart.xboxlive.com/7/81a21000-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080"
+		},
+		{
+			firstname: "Simetierre",
+			imgSrc: "https://fr.web.img6.acsta.net/pictures/19/02/08/09/45/4614816.jpg"
+		},
+		{
+			firstname: "Ça",
+			imgSrc: "https://fr.web.img3.acsta.net/pictures/13/12/30/11/10/553375.jpg"
+		},
+		{
+			firstname: "L'orphelinat",
+			imgSrc: "https://fr.web.img3.acsta.net/c_310_420/medias/nmedia/18/64/33/97/18888814.jpg"
+		}
+	];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<Header />
+			<main>
+				{cardsData.map((card, index) => (
+					<Card key={index} firstname={card.firstname} imgSrc={card.imgSrc} />
+				))}
+			</main>
+		</>
+	);
 }
 
-export default App
+
+
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import "../assets/styles/Header.css";
+
+function Header() {
+	return (
+		<header>
+			
+			<nav>
+			
+			</nav>
+		</header>
+	);
+}
+
+export default Header;
